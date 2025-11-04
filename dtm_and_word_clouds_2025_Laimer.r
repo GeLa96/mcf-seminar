63b17e07bb2b6332183aa525768654fe6d6952b5
Gerhard Laimer <h12103931@s.wu.ac.at>
2025-11-04 19:43:20 +0100

    W

--- dtm_and_word_clouds_2025_Laimer.r
+++ dtm_and_word_clouds_2025_Laimer.r
@@ -20,7 +20,7 @@ QandAs <- read.csv2("/Users/gerhardlaimer/Desktop/mcf-seminar/QandAs.csv",
 
 # set if statements or QandAs should be analyzed
 to_corpus <- data.frame(cbind(statements$unidate, statements$modtxt)) # column binds by unidate and modtxt
-# to_corpus<-data.frame(cbind(QandAs$unidate, QandAs$modtxt))
+# to_corpus <- data.frame(cbind(QandAs$unidate, QandAs$modtxt))
 
 names(to_corpus) <- c("doc_id", "text")
 to_corpus$doc_id <- as.character(to_corpus$doc_id)
@@ -29,7 +29,7 @@ to_corpus$text <- as.character(to_corpus$text)
 to_corpus <- tm::DataframeSource(to_corpus)
 
 corpus <- VCorpus(to_corpus, readerControl = list(language = "english"))
-View(corpus)
+# View(corpus)
 
 NLP_tokenizer <- function(x) {
     unlist(lapply(ngrams(words(x), 1:2), paste, collapse = "_"), use.names = FALSE)
@@ -39,7 +39,7 @@ dtm <- DocumentTermMatrix(corpus,
     control = list(
         removeNumbers = TRUE,
         # stopwords = c("FALSE", "false", "fals"),
-        tokenize = NLP_tokenizer, #
+        tokenize = NLP_tokenizer,
         stopwords = TRUE,
         tolower = TRUE,
         stemming = TRUE,
@@ -47,7 +47,7 @@ dtm <- DocumentTermMatrix(corpus,
     )
 )
 
-dtm <- removeSparseTerms(dtm, 0.98)
+dtm <- removeSparseTerms(dtm, 0.90)
 
 dtm_matrix <- as.matrix(dtm)
 dimnames(dtm_matrix) == "false"
@@ -56,15 +56,38 @@ cloud_frame <- data.frame(word = names(dtm_words), freq = dtm_words)
 cloud_frame <- cloud_frame[cloud_frame$word != "fals", ]
 # edit(cloud_frame)
 
+# to save the image wrap png() and dev.off() around set.seed() and the wordcloud
+png(
+    filename = "/Users/gerhardlaimer/Desktop/mcf-seminar/plots_mini1/wordcloud_statements_v4.png",
+    width = 1600, height = 1200, res = 200
+)
+
 set.seed(12)
 wordcloud(
     words = cloud_frame$word, freq = cloud_frame$freq, min.freq = 5,
     max.words = 400, random.order = FALSE, rot.per = 0.35,
-    colors = brewer.pal(8, "Dark2")
+    colors = brewer.pal(8, "Dark")
 )
 
+dev.off() # closes png !!!
 
-###### Creatin own word clouds
-## 1) use statemnt.csv and do not filter for fals
+###### Creating own word clouds
+## 1) Statements data
+# unigrams
+# no stemming
+# max.words = 200
 
-# comment out : cloud_frame <- data.frame(word = names(dtm_words), freq = dtm_words)
+## 2)
+# exclude this code line : cloud_frame <- cloud_frame[cloud_frame$word != "fals", ]
+# stemming on
+# unigrmas
+
+## 3) QandAs data
+# max.words = 400
+# colors = brewer.pal(12, "Paired")
+# rot.per = 0.5
+
+## 4)
+# colors = brewer.pal(9, "Set1")
+# removeSparseTerms(dtm, 0.90)
+# ngrams(words(x), 3:4)
